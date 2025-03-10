69228e3b8bc01d271cf95eaed9f267430c6d7950
Memlith <iamcaiosimonassi@gmail.com>
2025-03-10 10:52:06 -0300

    feat: add aula 01 presencial

---
+++ 02_semestre/tec_prog/01_lista_poo/01_desafio.py
@@ -0,0 +1,7 @@
+"""
+Desafio 1: Cadastro de Produtos
+
+*   Crie uma classe Produto com os atributos id_produto, nome e preco.
+*   Utilize um dicionário para armazenar os produtos.
+*   Permita adicionar e buscar produtos pelo ID.
+"""
